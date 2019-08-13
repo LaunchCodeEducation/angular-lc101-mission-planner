4c035bb29399ab83ad8449fd192b08e7e3b43303
Blake Mills <welzie@gmail.com>
2019-08-13 13:06:18 -0500

    removed countries component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CrewComponent } from './crew/crew.component';
-import { CountriesComponent } from './countries/countries.component';
 import { EquipmentComponent } from './equipment/equipment.component';
 import { ExperimentsComponent } from './experiments/experiments.component';
 
@@ -13,7 +12,6 @@ import { ExperimentsComponent } from './experiments/experiments.component';
     AppComponent,
     HeaderComponent,
     CrewComponent,
-    CountriesComponent,
     EquipmentComponent,
     ExperimentsComponent
   ],
