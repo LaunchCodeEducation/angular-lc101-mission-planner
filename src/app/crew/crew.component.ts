bea531f614102fd0a285c2124ec4cd4e03370157
Jim Flores <jimflores@Jims-MacBook-Air.local>
2019-08-30 12:18:30 -0500

    Added *ngFor to equipment and experiments columns.

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CrewComponent implements OnInit {
 
   memberBeingEdited: object = null;
+  inCrew: boolean = false;
 
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
@@ -21,7 +22,15 @@ export class CrewComponent implements OnInit {
   }
 
   add(memberName: string, isFirst: boolean) {
-    this.crew.push({name: memberName, firstMission: isFirst});
+    for (let i=0; i < this.crew.length; i++) {
+        if(this.crew[i]['name'] === memberName) {
+            this.inCrew = true;
+        }
+    }
+    if (!this.inCrew) {
+        this.crew.push({name: memberName, firstMission: isFirst});
+    }
+    this.inCrew = false;
   }
 
   remove(member: object) {
