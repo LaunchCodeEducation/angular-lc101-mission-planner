a465bca17041b34aefc55e65fbf5d7ec26fc98f5
Brenna McGahan <brenna.degan@gmail.com>
2020-03-30 20:03:31 -0500

    studio angular 2

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -13,9 +13,28 @@ export class CrewComponent implements OnInit {
     {name: "Ellen Ochoa", firstMission: true}
   ];
 
+  memberBeingEdited: object = null;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+ save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
+}
 }
