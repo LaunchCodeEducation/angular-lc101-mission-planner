dd3a8c061a4b4b0bfba30df9a2367330f7ce6b41
paulwilliams <paalpwmd>
2020-07-27 19:32:35 -0500

    complete assignment

---
+++ mission-planning-dashboard/src/app/crew/crew.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-crew",
+  templateUrl: "./crew.component.html",
+  styleUrls: ["./crew.component.css"],
+})
+export class CrewComponent implements OnInit {
+  crew: string[] = ["Jessica Watkins", "Raja Chari", "Jasmin Moghbeli"];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
