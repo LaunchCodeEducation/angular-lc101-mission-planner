4614109020e5c7ce528626a41265866b7d88d55d
paulwilliams <paalpwmd>
2020-07-29 20:01:44 -0500

    complete studio

--- mission-planning-dashboard/src/app/crew/crew.component.ts
+++ mission-planning-dashboard/src/app/crew/crew.component.ts
@@ -6,9 +6,29 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./crew.component.css"],
 })
 export class CrewComponent implements OnInit {
-  crew: string[] = ["Jessica Watkins", "Raja Chari", "Jasmin Moghbeli"];
+  crew: object[] = [
+    { name: "Eileen Collins", firstMission: false },
+    { name: "Mae Jemison", firstMission: false },
+    { name: "Ellen Ochoa", firstMission: true },
+  ];
+  memberBeingEdited: object = null;
 
   constructor() {}
 
   ngOnInit() {}
+
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({ name: memberName, firstMission: isFirst });
+  }
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+  save(name: string, member: object) {
+    member["name"] = name;
+    this.memberBeingEdited = null;
+  }
 }
--- mission-planning-dashboard/src/app/header/header.component.ts
+++ mission-planning-dashboard/src/app/header/header.component.ts
@@ -8,8 +8,18 @@ import { Component, OnInit } from "@angular/core";
 export class HeaderComponent implements OnInit {
   missionName: string = "Mars 2030";
   rocketName: string = "Plasma Max";
+  editingMission: boolean = false;
+  editingRocket: boolean = false;
 
   constructor() {}
 
   ngOnInit() {}
+  updateMission(updatedName: string) {
+    this.missionName = updatedName;
+    this.editingMission = false;
+  }
+  updateRocket(updatedName: string) {
+    this.rocketName = updatedName;
+    this.editingRocket = false;
+  }
 }
