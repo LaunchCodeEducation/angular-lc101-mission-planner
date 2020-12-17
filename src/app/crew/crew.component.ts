800f6b92f2ab1eb81841b274dacac554b6f469ab
rsunithat <rsunithat@gmail.com>
2020-12-17 12:16:24 -0600

    studio-2

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -12,7 +12,22 @@ export class CrewComponent implements OnInit {
     {name: "Mae Jemison", firstMission: false},
     {name: "Ellen Ochoa", firstMission: true}
   ];
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+  memberBeingEdited: object = null;
 
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+ save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
+  }
   constructor() { }
 
   ngOnInit() {
