9961c8c0ddbd8b0f54dae620cfcc494728f62d51
Nate Steil <nathanrsteil@gmail.com>
2021-04-09 17:10:07 -0500

    completed all steps, added the edit ability to crew members

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -13,6 +13,8 @@ export class CrewComponent implements OnInit {
     {name: "Ellen Ochoa", firstMission: true}
   ];
 
+  memberBeingEdited: object = null;
+
   constructor() { }
 
   ngOnInit() {
@@ -22,4 +24,17 @@ export class CrewComponent implements OnInit {
     this.crew.push({name: memberName, firstMission: isFirst});
   }
   
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+ save (name: string, member: object) {
+   member['name'] = name;
+   this.memberBeingEdited = null;
+ }
 }
