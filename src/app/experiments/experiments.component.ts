7e2d35f13d9ff69b4da186e3d32b06070321f64c
Nate Steil <nathanrsteil@gmail.com>
2021-04-09 19:45:42 -0500

    added the function for remove experiments

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experiments: string[] = [
+    "Mars soil sample",
+    "Plant growth in habitat",
+    "Human bone density"
+  ]
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(experiment:string) {
+    if(!this.experiments.includes(experiment)) {
+      this.experiments.push(experiment)
+    }
+  }
+
+  remove(experiment: string) {
+    let index = this.experiments.indexOf(experiment);
+    this.experiments.splice(index,1)
+  }
 }
