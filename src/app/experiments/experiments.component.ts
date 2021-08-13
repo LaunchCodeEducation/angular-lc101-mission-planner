ddd61333708b1eae1dbc77ddf2969e17536333d2
Hope Ciuffa <hopeciuffa@gmail.com>
2021-08-13 10:11:37 -0500

    experiments added

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
    experiments: string[] = ['Moon soil sample', 'Plant growth in orbit', 'Human bone density changes', 'Water droplet podcast for grades K-5', 'Satellite launch'];
-
+  added: string[] =[];
    constructor() { }
 
    ngOnInit() { }
-
+addExperiment(experiment){
+  if(this.added.length<3){
+    this.added.push(experiment);
+    }
+  }
 }
